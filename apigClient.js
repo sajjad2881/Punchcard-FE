d1c0a710055bb96c638ae53902a9dfd061320a80
Sanjay Rajasekharan <sanjay.rajasekharan@gmail.com>
2023-05-07 16:08:30 -0400

    API gateway update + search UI

--- apigClient.js
+++ apigClient.js
@@ -446,13 +446,13 @@ apigClientFactory.newClient = function (config) {
     apigClient.searchRestaurantsGet = function (params, body, additionalParams) {
         if(additionalParams === undefined) { additionalParams = {}; }
         
-        apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
+        apiGateway.core.utils.assertParametersDefined(params, ['location', 'query'], ['body']);
         
         var searchRestaurantsGetRequest = {
             verb: 'get'.toUpperCase(),
             path: pathComponent + uritemplate('/search/restaurants').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
             headers: apiGateway.core.utils.parseParametersToObject(params, []),
-            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, ['location', 'query']),
             body: body
         };
         
